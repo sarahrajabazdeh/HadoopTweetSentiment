40b30bc0686720c6f91d4281687afecf1ec81a04
Mohsin Mushtaq <mmushtaq@sorint.it>
2024-03-12 02:44:35 +0100

    fixed the initialization script

--- shell_scripts/initialization.sh
+++ shell_scripts/initialization.sh
@@ -6,7 +6,8 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-file_path="$1"
+file_path=$1
+default_dir=/user/datascience
 
 # # Check if the file path provided exists
 if [ ! -f "$file_path" ]; then
@@ -15,10 +16,10 @@ if [ ! -f "$file_path" ]; then
 fi
 
 # This script is used to initialize the HDFS file system
-hadoop fs -test -d user || hadoop fs -mkdir user
-hadoop fs -test -d user/data || hadoop fs -mkdir user/data
-hadoop fs -test -d user/data/kaggle || hadoop fs -mkdir user/data/kaggle
-hadoop fs -test -d user/data/processed || hadoop fs -mkdir user/data/processed
+
+hadoop fs -test -d $default_dir/data || hadoop fs -mkdir $default_dir/data
+hadoop fs -test -d $default_dir/data/kaggle || hadoop fs -mkdir $default_dir/data/kaggle
+hadoop fs -test -d $default_dir/data/processed || hadoop fs -mkdir $default_dir/data/processed
 
 # Copy the Kaggle dataset to HDFS from the given path
-hadoop fs -test -e user/data/tweets.csv || hadoop fs -put "$file_path" user/data/tweets.csv
+hadoop fs -test -e $default_dir/data/kaggle/tweets.csv || hadoop fs -put "$file_path" $default_dir/data/kaggle
