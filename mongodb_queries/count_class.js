a64e96d8204bd49fbbcdfb8dde2b301e63919982
sarahrajabzadeh <khode.divergent@gmail.com>
2024-06-05 19:49:08 +0200

    fix the query

--- mongodb_queries/count_class.js
+++ mongodb_queries/count_class.js
@@ -5,11 +5,9 @@
 // Connect to the database ds_project
 db = connect('mongodb://localhost/ds_project');
 
+// Get the count of documents in each class
 var classCounts = db.tweets.aggregate([
   { $group: { _id: "$target", count: { $sum: 1 } } }
 ]).toArray();
 
 printjson(classCounts);
-
-printjson(classCounts.length);
-printjson(disctionary.length);  
