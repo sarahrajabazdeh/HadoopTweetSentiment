b12a2e83923aa2870b173f5a9b1f101be6058819
sarahrajabzadeh <khode.divergent@gmail.com>
2024-06-19 16:27:16 +0200

    update the query

--- mongodb_queries/count_class.js
+++ mongodb_queries/count_class.js
@@ -7,7 +7,11 @@ db = connect('mongodb://localhost/ds_project');
 
 // Get the count of documents in each class
 var classCounts = db.tweets.aggregate([
-  { $group: { _id: "$target", count: { $sum: 1 } } }
+  { $group: { _id: "$target", count: { $sum: 1 } } },
+  { $project: { target: "$_id",  count: 1, _id: 0 } }
 ]).toArray();
 
 printjson(classCounts);
+
+
+
